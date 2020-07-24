ca7aac9f37e5d162a0ae8c53356d660f98d235a8
Ivan <ivankuz@hotmail.com>
2020-07-23 22:19:19 -0700

    .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,4 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
-  overrides: [
-    {
-      excludedFiles: '*.js',
-    },
-  ],
 };
