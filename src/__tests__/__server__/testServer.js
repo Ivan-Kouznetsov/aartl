ae3284cced81d8564aaed7d9abf3d49cd6f312a5
Ivan <ivankuz@hotmail.com>
2020-07-24 01:31:25 -0700

    convert test server to js

--- src/__tests__/__server__/testServer.js
+++ src/__tests__/__server__/testServer.js
@@ -1,5 +1,5 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.raw({ type: '*/*' }));
