a86a66c50f5c48477770b876deb67b4d3497fe34
Ivan <ivankuz@hotmail.com>
2020-07-29 20:03:19 -0700

    update a unit test's expected error message

--- src/__tests__/unit/validator.spec.ts
+++ src/__tests__/unit/validator.spec.ts
@@ -9,9 +9,7 @@ describe('Validator', () => {
     expect(getFirstValidationError(fixtures.passOn)).toEqual('Cannot have passed on values in last request');
     expect(getFirstValidationError(fixtures.waitInLast)).toEqual('Cannot have a wait in last request');
     expect(getFirstValidationError(fixtures.tooManyArgs)).toContain('has too many arguments');
-    expect(getFirstValidationError(fixtures.hasNullRules)).toContain(
-      'should return stuff when queried has inavlid lines at the end'
-    );
+    expect(getFirstValidationError(fixtures.hasNullRules)).toContain('has one or more invalid lines at the end');
   });
 
   it('should validate valid test', () => {
