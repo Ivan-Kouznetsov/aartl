fc9ea555f212c25864e6e793fc2caf807ae2d9a5
Ivan <ivankuz@hotmail.com>
2020-08-06 08:29:17 -0700

    check for not found json paths

--- src/runner/runner.ts
+++ src/runner/runner.ts
@@ -123,7 +123,7 @@ export const runTest = async (test: ITest): Promise<ITestResult> => {
           const data = jsonPath(json, rule.jsonpath);
 
           if (typeof rule.rule === 'function') {
-            const nonCompliantValue = rule.rule(data ? data : []);
+            const nonCompliantValue = Array.isArray(data) ? rule.rule(data) : 'nothing';
             if (nonCompliantValue !== undefined) {
               failReasons.push(
                 `Expected ${rule.jsonpath} to be ${rule.originalRule}, got ${nonCompliantValue.toString()}`
