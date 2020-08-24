ceb0d391db669051077b79225930bf9b98f2a18b
Ivan-Kouznetsov <ivankuz@hotmail.com>
2020-08-23 23:58:34 -0700

    add preprocessor

---
+++ src/parser/preprocessor.ts
@@ -0,0 +1,26 @@
+import * as fileSystem from 'fs';
+import { resolve } from 'path';
+
+const getFixture = (name: string, path: string): string => {
+  try {
+    const filePath = resolve(path, 'fixtures', name + '.fixture');
+    return fileSystem.readFileSync(filePath).toString('utf-8');
+  } catch (ex) {
+    throw `Fixture file ${name}.fixture not found`;
+  }
+};
+
+export const applyFixtures = (text: string, path: string): string => {
+  const keyword = 'body from fixture:';
+  const bodyFromFixtures = /(?<=body from fixture:\s{0,}).+/g;
+  const fixtures = text.match(bodyFromFixtures) ?? [];
+
+  fixtures.forEach((f) => {
+    const fixtureName = f.trim();
+    const textToReplace = new RegExp(`${keyword}\\s{0,}${fixtureName}`, 'g');
+    const replacementText = `body: ${getFixture(fixtureName, path)}`;
+    text = text.replace(textToReplace, replacementText);
+  });
+
+  return text;
+};
