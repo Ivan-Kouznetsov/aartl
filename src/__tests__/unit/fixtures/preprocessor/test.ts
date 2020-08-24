5f28ff891e23a787777d754b357782907a951260
Ivan Kouznetsov <52215050+Ivan-Kouznetsov@users.noreply.github.com>
2020-08-24 00:23:36 -0700

    Fixtures (#72)
    * add preprocessor
    
    * use applyFixtures in cli

---
+++ src/__tests__/unit/fixtures/preprocessor/test.ts
@@ -0,0 +1,6 @@
+export const test = `Test that it should get data
+Expect HTTP request
+    url: http://localhost:3000/
+    method: POST
+    body from fixture: newpost
+To respond with status code 200`;
---
+++ src/__tests__/unit/preprocessor.spec.ts
@@ -0,0 +1,28 @@
+import { test } from './fixtures/preprocessor/test';
+import { applyFixtures } from '../../parser/preprocessor';
+import * as path from 'path';
+
+describe('Preprocessor', () => {
+  it('should replace body from fixture:', () => {
+    const result = applyFixtures(test, path.resolve('./src/__tests__/unit//fixtures/preprocessor/')).replace('\r', '');
+    expect(result).toEqual(
+      'Test that it should get data\nExpect HTTP request\n    url: http://localhost:3000/\n    method: POST\n    body: This is a\nnew post\nTo respond with status code 200'
+    );
+  });
+
+  it('should throw when file is not found', () => {
+    expect(() => {
+      applyFixtures(test, path.resolve('./'));
+    }).toThrow();
+  });
+
+  it('should not replace body from fixture: when it is not present', () => {
+    const result = applyFixtures(
+      test.replace(' from fixture', ''),
+      path.resolve('./src/__tests__/unit//fixtures/preprocessor/')
+    ).replace('\r', '');
+    expect(result).toEqual(
+      'Test that it should get data\nExpect HTTP request\n    url: http://localhost:3000/\n    method: POST\n    body: newpost\nTo respond with status code 200'
+    );
+  });
+});
