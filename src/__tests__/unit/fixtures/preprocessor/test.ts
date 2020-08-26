269be01da6fbadd079cc2ffb86695c3fab2c2b61
Ivan-Kouznetsov <ivankuz@hotmail.com>
2020-08-25 19:42:27 -0700

    add test for caching

--- src/__tests__/unit/fixtures/preprocessor/test.ts
+++ src/__tests__/unit/fixtures/preprocessor/test.ts
@@ -4,3 +4,25 @@ Expect HTTP request
     method: POST
     body from fixture: newpost
 To respond with status code 200`;
+
+export const multipleUseOfFixture = `Test that it should get data
+Expect HTTP request
+    url: http://localhost:3000/
+    method: POST
+    body from fixture: newpost
+To respond with status code 200
+
+Test that it should get data
+Expect HTTP request
+    url: http://localhost:3000/
+    method: POST
+    body from fixture: newpost
+To respond with status code 200
+
+Test that it should get data
+Expect HTTP request
+    url: http://localhost:3000/
+    method: POST
+    body from fixture: newpost
+To respond with status code 200
+`;
